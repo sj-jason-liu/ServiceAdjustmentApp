107ea81595902e5b64f9340764cae3277c5cad96
sj-jason-liu <sj.jason.liu@gmail.com>
2021-09-05 14:56:27 +0800

    Created scripts to all panel(except Main Menu) with IPanel interface implemented.

---
+++ Assets/Insurance App/assets/Scripts/Panel/ClientInfoPanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientInfoPanel : MonoBehaviour, IPanel
+{
+    public Text caseNumberText;
+    public InputField firstNameInput, lastNameInput;
+
+    public void ProcessInfo()
+    {
+        
+    }
+}
---
+++ Assets/Insurance App/assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("UIManager is NULL");
+            }
+
+            return _instance;
+        }
+    }
+
+    public Case activeCase;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void CreateNewCase()
+    {
+        activeCase = new Case();
+        int newCaseID = Random.Range(0, 999);
+        activeCase.caseID = newCaseID.ToString();
+    }
+}
