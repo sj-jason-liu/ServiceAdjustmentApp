b09c2ed8aa3d5b1ccc6ad174cf3f0b8888efa978
sj-jason-liu <sj.jason.liu@gmail.com>
2021-09-13 15:13:47 +0800

    Make SelectPanel can shows the imformation from server.

--- Assets/Insurance App/assets/Scripts/Panel/SelectPanel.cs
+++ Assets/Insurance App/assets/Scripts/Panel/SelectPanel.cs
@@ -7,6 +7,11 @@ public class SelectPanel : MonoBehaviour, IPanel
 {
     public Text infoText;
 
+    public void OnEnable()
+    {
+        infoText.text = UIManager.Instance.activeCase.name;
+    }
+
     public void ProcessInfo()
     {
         
